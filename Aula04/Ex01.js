caa64ed57481c17273fd2302b0163b56bd9ec887
Pedro Prá <pedro@pedropra.com>
2012-11-19 10:22:30 -0200

    Exercicio 01 - Funções

---
+++ Aula04/Ex01.js
@@ -0,0 +1,12 @@
+function maior(valor1, valor2) {
+	if(valor1>valor2) {
+		console.log("Valoe 1 maior.")
+		return valor1;
+	} else { 
+		console.log("Valoe 2 maior.")
+		return valor2;
+	}
+}
+var a;
+a = maior(1,2);
+console.log(a);
