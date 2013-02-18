87c8ba99accc49dcc172d4db6319ea95bf4936c5
Pedro Prá <pedro@pedropra.com>
2013-02-18 10:16:47 -0300

    Update yomama/script.js
    Estrutura completa. 
    Lembrar que o celulaClicada ja pega o valor.
    Trabalhar com o innerhtml para verificar celular vazia. 

--- yomama/script.js
+++ yomama/script.js
@@ -2,8 +2,56 @@
 
 /*Função que identifica que a celula foi clicada*/
 
-function clickCelula() {
+/*function clickCelula() {
 	var celula = document.getElementsByName("celula");
 	console.log(celula.id);
 }
+*/
+/* Yomama JS */
 
+/*Função que identifica que a celula foi clicada*/
+
+function clickCelula(event) {
+	var celulaClicada = event.target; // retorna o alvo do evento (no caso a célula clicada)
+	
+	if (estaVazia(celulaClicada)) {
+		return;
+	}
+	
+	if (ehVez("X")) {
+		preenche(celulaClicada, "X");
+	} else {
+		preenche(celulaClicada, "O");
+	}
+	
+	if (verificarVencedor()) {
+		aacbarJogo();
+	} else {
+		trocarVez();
+	}
+}
+
+
+function estaVazia(celula) {
+	// return true ou false
+}
+
+function ehVez(jogador) {
+	// return true ou false
+}
+
+function preenche(celula, jogador) {
+	// preencher a celula de algum jeito
+}
+
+function verificarVencedor() {
+	// return true ou false
+}
+
+function acabarJogo() {
+	// anunciar vencedor e resetar o jogo
+}
+
+function trocarVez() {
+	// efetua a troca da vez do jogador de X para O e viceversa
+}
